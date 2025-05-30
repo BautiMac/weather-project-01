be076e64872b779fb57fb1bc9a0d10997b9d9588
Bautista Machado <Bautistamachado777@gmail.com>
2025-05-30 11:56:55 -0300

    v1.1.2

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ document.querySelector('.temp').innerHTML = `${Math.round(data.main.temp)}&deg;C
 document.querySelector('.city').innerHTML = data.name;
 document.querySelector('.humidity').innerHTML = `${data.main.humidity}%`;
 document.querySelector('.wind').innerHTML = `${data.wind.speed}km/h`;
+weatherIcon.src = weatherIcons[data.weather[0].main.toLowerCase()] || 'images/rain.png';
 
 const weatherIcons = {
     clear: 'images/clear.png',
@@ -48,6 +49,7 @@ weather.src = weatherIcons[data.weather[0].main] || 'images/rain.png';
 
 weather.style.display = 'block';
 errorMessage.style.display = "none";
+chekWeather(inputbox.value.trim());
 
 
 }
